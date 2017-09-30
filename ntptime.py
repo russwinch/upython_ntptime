e6e052c5b6e1bec3fe77135d768b8466679543de
russ <russell.winch@gmail.com>
2017-09-30 19:20:34 +0100

    broke into functions, added padding function, catching errors when update fails

--- ntptime.py
+++ ntptime.py
@@ -1,4 +1,4 @@
-""" retrieves time from ntpserver and displays it on oled screen"""
+"""retrieves time from ntpserver and displays it on oled screen"""
 
 import time
 import ntptime
@@ -8,13 +8,29 @@ import ssd1306
 i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4))
 oled = ssd1306.SSD1306_I2C(64, 48, i2c) # width, height, pins
 
-ntptime.settime()
-while True:
-    print(time.localtime())
+def setTime():
+    try:
+        ntptime.settime()
+        print('succesfully updated time')
+    except OSError:
+        print('error, couldn\'t retrieve time')
 
+def pad(p):
+    if p < 10:
+        return '0' + str(p)
+    else:
+        return str(p)
+
+def printTime(h, m, s):
     oled.fill(0) # clear the screen
-    oled.text(str(time.localtime()[3]) + ':' + str(time.localtime()[4]) + ':' +
-            str(time.localtime()[5]), 0, 0)
+    oled.text(pad(h + 1) + ':' + pad(m) + ':' + pad(s), 0, 0) #hack for BST!!
     oled.show()
 
+setTime()
+
+while True:
+    print(time.localtime())
+    printTime(time.localtime()[3], time.localtime()[4], time.localtime()[5])
+
     time.sleep(1)
+
--- readme.md
+++ readme.md
@@ -5,8 +5,8 @@ assumes device already connected to wifi in the boot.py *add examples*
 
 todo(in order of priority:
 1. add DST/BST functionality
-1. add function to pad zeros
-1. add error catching when the ntptime.settime() fails (often)
+1. implement ticker to update in place of sleep
+1. implement delay and retry when update fails
 1. improve fonts and display
 1. add a button and timeout so the oled doesn't stay on forever and burn out
     
